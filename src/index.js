e1d71e04e168b744a3799df5c8d428e86f126b4a
Bernard <beniek12@onet.pl>
2023-02-05 14:09:03 +0100

    Added menu content populate function and styled menu for now

--- src/index.js
+++ src/index.js
@@ -4,27 +4,43 @@ import Pizza from "./pizza.jpeg";
 import RestaurantImg from "./restaurant.jpg";
 import { mainPage } from "/src/main-page.js";
 import { homeContent } from "/src/home.js";
+import { createMenu } from "/src/menu.js";
 
 export const restaurantInfo = (function () {
   const name = "Some restaurant";
   const menu = [
     {
-      category: "asd",
+      category: "Pasta",
+      description:
+        "Tagliolini al nero di seppia, tomato, prawns, mussels, garlic & white wine",
+      price: "85zł",
     },
     {
-      category: "asd",
+      category: "Meat",
+      description:
+        "Lamb chops, roasted seasonal vegetables, potatoes puree, red wine sauce ",
+      price: "125zł",
     },
     {
-      category: "asd",
+      category: "Fish And Seafood",
+      description:
+        "Gamberoni piccanti, fried prawns, tomato, garlic & parsley ",
+      price: "90zł",
     },
     {
-      category: "asd",
+      category: "Sides",
+      description: "Broccoli sautéed",
+      price: "25zł",
     },
     {
-      category: "asd",
+      category: "Desserts",
+      description: "Tiramisu, ladyfingers, Kahula, coffee",
+      price: "40zł",
     },
     {
-      category: "asd",
+      category: "Sweet Wine",
+      description: "Moscato D’Astin DOCG, Italy USA 750 ml ",
+      price: "220zł",
     },
   ];
 
@@ -58,6 +74,7 @@ content.classList.add("background-image");
 function init() {
   mainPage();
   homeContent();
+  createMenu();
 }
 
 init();
--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,37 @@
 import createElement from "/src/dom.js";
 import { restaurantInfo } from ".";
+import { clearContentDiv } from "/src/index.js";
 
 export function createMenu() {
   const menu = restaurantInfo.showMenu();
-  console.log(menu);
+  const wrapper = createElement("div", "");
+  const contentInner = createElement("div", "");
+
+  wrapper.append(contentInner);
+
+  menu.forEach((danie) => {
+    const menuCard = createElement("div", "");
+    const h2 = createElement("h2", `${danie.category}`);
+    const description = createElement("p", `${danie.description}`);
+    const price = createElement("p", `${danie.price}`);
+
+    menuCard.classList.add("margin-top", "menuCard");
+    contentInner.classList.add("menu-grid");
+
+    menuCard.append(h2, description, price);
+    contentInner.append(menuCard);
+    return menuCard;
+  });
+
+  wrapper.classList.add("menu", "wrapper");
+
+  return wrapper;
+}
+
+export function menuContent() {
+  const menu = createMenu();
+  const content = document.getElementById("content");
+
+  clearContentDiv();
+  content.append(menu);
 }
